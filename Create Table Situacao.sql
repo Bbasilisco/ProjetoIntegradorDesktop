51ec10608aee71bc21716f72df8628ba3dea1592
Beatriz Basilisco <beatrizfonsecabasilisco@gmail.com>
2026-08-19 13:02:15 +0000

    deletando arquivos para reenviar mais organizado

--- Create Table Situacao.sql
+++
@@ -1,7 +0,0 @@
-create table tblSituacao (
-id int identity primary key,
-situacao varchar(50),
-idUsuario int foreign key (idUsuario) references tblUsuario
-)
-
-insert into tblSituacao (situacao ) values ('Agendado'),('Efetivado'),('Cancelado')
